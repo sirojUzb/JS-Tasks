9db629656d571e07c59d795d6e89144072091384
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-12-23 23:07:05 +0900

    function example

--- task2.js
+++ task2.js
@@ -1,1 +1,9 @@
 // I'll revise last tasks
+const user = {
+  name: "John",
+  surName: "Doe",
+  getFullName: function () {
+    return `${this.name} ${this.surName}`;
+  },
+};
+console.log(user.getFullName());
