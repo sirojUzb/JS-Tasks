cb0bc6547d9ba59b295325199361521223d04b5b
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-04-18 23:58:15 +0900

    stage_1 variables numbers string

--- stage1.js
+++ stage1.js
@@ -4,4 +4,20 @@ let age = 34;
 
 age = 34;
 
-console.log("Hi, my name is ${name} and I am ${age} years old");
+console.log(`Hi, my name is ${name} and I am ${age} years old`);
+
+const a = 10;
+const b = 5;
+
+console.log(`The sum is ${a + b}`);
+
+const x = 20;
+const y = 7;
+
+console.log(`The differance is ${x - y}`);
+
+// String
+const FirstName = "John";
+const SecondName = "Joe";
+
+console.log(`FullName: ${FirstName} ${SecondName}`);
