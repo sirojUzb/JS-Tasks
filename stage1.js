7917676af39ff9703cc7eb927e7abf142f0cdcd3
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-04-30 21:37:32 +0900

    revise

--- stage1.js
+++ stage1.js
@@ -11,11 +11,13 @@ const a = 10;
 const b = 5;
 
 console.log(`The sum is ${a + b}`);
+
 //subtrack of find the difference
 const x = 20;
 const y = 7;
 
 console.log(`The differance is ${x - y}`);
+
 // multiply to get the area
 const length = 8;
 const width = 3;
@@ -38,19 +40,31 @@ let num1 = 70;
 let num2 = 80;
 let num3 = 90;
 
+function averegeNum() {
+  console.log(`averegeNum ${(num1 + num2 + num3) / 3}`);
+  return;
+}
+averegeNum();
+
 // STRING
 
 const FirstName = "John";
 const SecondName = "Joe";
 
 console.log(`FullName: ${FirstName} ${SecondName}`);
+
 // 3.Make it Uppercase
 let language = "javascript";
 
 console.log(`language = ${language.toUpperCase()}`);
+
+let sentence = "I love python";
+let upSentence = sentence.replace("python", "javascript");
+console.log(upSentence);
 
 let is_even = function is_even(number) {
   return number % 2 === 0;
 };
 
 console.log(is_even(4));
+// revise
