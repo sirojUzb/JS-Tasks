61989468cab1b1a47c7bb1b5d0cae8142fa1df27
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-08-17 00:06:56 +0900

    binary

--- javascript.js
+++ javascript.js
@@ -4,3 +4,24 @@ var age1 = 18;
 var age2 = "18";
 
 console.log(age1 == age2);
+// true
+console.log(age1 === age2);
+// false
+
+// == / === - more strict-"data type" bilan tekshiradi.
+
+// > - katta < - kichik
+
+// && - and(va) operator
+true && false => false
+false && true => false
+false && false => false
+true && true => true
+
+// || - or(yoki) operator
+true || false => true
+false || true =>  true
+true || true => true
+false || false => false
+
+// Binary --> 0 va 1
