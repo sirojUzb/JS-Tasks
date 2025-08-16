ad13c4e5ad25eb50464a8ea1749cd2d55778eae3
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-08-16 23:56:26 +0900

    conditional operations

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
-// Date types
-// primative
-// non-primative
-Math;
-string;
+// Conditional operations
+// Conditional operations return boolean.
+var age1 = 18;
+var age2 = "18";
+
+console.log(age1 == age2);
