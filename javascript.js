edfd17087f4d66bba3440b111ff27fbce75be017
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-08-22 22:56:52 +0900

    if/else

--- javascript.js
+++ javascript.js
@@ -13,22 +13,24 @@ console.log(age1 === age2);
 // > - katta < - kichik
 
 // && - and(va) operator
-true && false => false
-false && true => false
-false && false => false
-true && true => true
+true && false - false;
+false && true - false;
+false && false - false;
+true && true - true;
 
 // || - or(yoki) operator
-true || false => true
-false || true =>  true
-true || true => true
-false || false => false
+true || false - true;
+false || true - true;
+true || true - true;
+false || false - false;
 
 // Binary --> 0 va 1
-var age=25;
+var age = 25;
 
-if(age>=18 && age<=19){
-    console.log('you might enter to the club');
-}else if(age>=20&&age<=30){console.log('you can enroll to the premium account for free!');
-}else{console.log('you are not allowed!');
+if (age >= 18 && age <= 19) {
+  console.log("you might enter to the club");
+} else if (age >= 20 && age <= 30) {
+  console.log("you can enroll to the premium account for free!");
+} else {
+  console.log("you are not allowed!");
 }
