ad486fe379452bb1905631ac7153df9ddb97a290
sirojUz <sirojiddinmamatovuzb@gmail.com>
2025-11-12 22:15:40 +0900

    freeze/seal

--- javascript.js
+++ javascript.js
@@ -123,4 +123,4 @@ for (let i = 0; i < entries.length; i++) {
 }
 // for security, there are Object.freeze and Object.seal
 // in freeze you cannot create, update and delete. freeze is stronger than seal
-// in seal, it allows only the modification(update)
+// in seal, it allows only the modification(updating)
